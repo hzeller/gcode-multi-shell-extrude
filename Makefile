9ab649a5a85cee270302366ddade151784c60b3c
Henner Zeller <h.zeller@acm.org>
2014-08-23 12:41:37 -0700

    o do offset the CGAL way.

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
-CXXFLAGS=-Wall -O2
-LIBS=-lm
+CXXFLAGS=-Wall -O2 -frounding-math
+LIBS=-lm -lCGAL_Core -lCGAL -lboost_thread -lmpfr -lgmp
 OBJECTS=multi-shell-extrude.o rotational-polygon.o polygon-offset.o
 
 multi-shell-extrude: $(OBJECTS)
 	$(CXX) $(CXXFLAGS) -o $@ $^ $(LIBS)
 
 %.o : %.cc
-	$(CXX) $(CXXFLAGS) -c -o $@ $< $(LIBS)
+	$(CXX) $(CXXFLAGS) -c -o $@ $<
 
 clean:
 	rm -f multi-shell-extrude $(OBJECTS)
--- polygon-offset.cc
+++ polygon-offset.cc
@@ -5,13 +5,36 @@
 
 #include "multi-shell-extrude.h"
 
+#include <stdio.h>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Polygon_2.h>
+#include <CGAL/create_offset_polygons_2.h>
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel CG_K ;
+typedef CG_K::FT                        CG_FT;
+typedef CG_K::Point_2                   CG_Point ;
+typedef CGAL::Polygon_2<CG_K>           CG_Polygon_2 ;
+typedef CGAL::Straight_skeleton_2<CG_K> CG_Ss ;
+typedef boost::shared_ptr<CG_Polygon_2> CG_PolygonPtr ;
+typedef std::vector<CG_PolygonPtr> CG_PolygonVector;
+
 Polygon PolygonOffset(const Polygon &polygon, double offset) {
-    Polygon result;
-    for (std::size_t i = 0; i < polygon.size(); ++i) {
-        const Point &p = polygon[i];
-        double from_center = distance(p.x, p.y, 0);
-        double stretch = (from_center + offset) / from_center;
-        result.push_back(Point(p.x * stretch, p.y * stretch));
-    }
-    return result;
+  CG_Polygon_2 cg_poly;
+  for (std::size_t i = 0; i < polygon.size(); ++i) {
+    const Point &p = polygon[i];
+    cg_poly.push_back(CG_Point(p.x, p.y));
+  }
+  cg_poly.push_back(CG_Point(polygon[0].x, polygon[0].y));
+  CG_FT cg_offset = offset;
+  
+  CG_PolygonVector cg_outer
+    = CGAL::create_exterior_skeleton_and_offset_polygons_2(cg_offset, cg_poly);
+  
+  Polygon result;
+  CG_Polygon_2 &cg_out_poly = *cg_outer[1];  // [0] contains bounding box (?)
+  for (int i = (int)cg_out_poly.size() - 1; i >= 0; --i) {
+    result.push_back(Point(cg_out_poly[i].x(), cg_out_poly[i].y()));
+  }
+
+  return result;
 }
